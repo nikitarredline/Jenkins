758801ff45821879333e2f49c2af374b18054c87
nikitarredline <povarsemen@yandex.ru>
2026-04-24 22:42:37 +0300

    practice

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -14,13 +14,17 @@ node() {
     }
 
     stage('Create config.ini') {
-        withCredentials([usernamePassword(credentialsId: "jenkins", usernameVariable: "user", passwordVariable: 'pass')]) {
+        withCredentials([usernamePassword(
+                credentialsId: "jenkins",
+                usernameVariable: "USER",
+                passwordVariable: "PASS"
+        )]) {
             sh '''
 cat > config.ini << EOF
 [jenkins]
 url=http://89.124.113.71/jenkins/
-user=$user
-password=$pass
+user=$USER
+password=$PASS
 
 [job_builder]
 recursive=True
@@ -30,39 +34,45 @@ EOF
         }
     }
 
-    stage('Debug host') {
+    stage('Host debug') {
         sh '''
+echo "=== HOST INFO ==="
 echo "WORKSPACE=$WORKSPACE"
+
 ls -la $WORKSPACE
 ls -la $WORKSPACE/jobs
 ls -la $WORKSPACE/config.ini
 '''
     }
 
-    stage('Run JJB') {
+    stage('Run Jenkins Job Builder') {
         sh '''
 docker run --rm \
   -v $WORKSPACE:/workspace \
   -w /workspace \
-  python:3.10 bash -c "
+  python:3.10 bash -c '
     set -e
 
-    echo '=== INSIDE CONTAINER ==='
+    echo "=== INSIDE CONTAINER ==="
     pwd
     ls -la
+
+    echo "=== CHECK MOUNT ==="
     ls -la config.ini
     ls -la jobs
 
-    echo '=== PYTHON ==='
+    echo "=== PYTHON VERSION ==="
     python --version
 
-    echo '=== INSTALL ==='
+    echo "=== INSTALL JJB ==="
     pip install --no-cache-dir jenkins-job-builder==5.0.3
 
-    echo '=== RUN JJB ==='
+    echo "=== JJB VERSION ==="
     jenkins-jobs --version
+
+    echo "=== RUN UPDATE ==="
     jenkins-jobs --conf config.ini update jobs/
-"
+'
 '''
     }
 }
