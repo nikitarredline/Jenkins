d7cb635e254960acc1b29f551c32b10359e6729a
nikitarredline <povarsemen@yandex.ru>
2026-04-24 22:58:13 +0300

    practice

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -1,54 +1,28 @@
 pipeline {
-
-    stage('DEBUG NODE') {
-        steps {
-            sh '''
-            echo "NODE NAME: $(hostname)"
-            whoami
-            pwd
-            ls -la
-        '''
-        }
-    }
-
-
     agent any
-
-    environment {
-        DOCKER_IMAGE = "python:3.10"
-    }
 
     stages {
 
-        sh '''
-    HOSTNAME=$(hostname)
-    echo "HOST=$HOSTNAME"
-'''
-
-        stage('Checkout') {
+        stage('DEBUG NODE') {
             steps {
-                checkout scm
+                sh '''
+                    echo "HOST=$(hostname)"
+                    echo "WORKSPACE=$WORKSPACE"
+                    ls -la
+                '''
             }
         }
 
         stage('Create config.ini') {
-
-            sh '''
-    HOSTNAME=$(hostname)
-    echo "HOST=$HOSTNAME"
-'''
-
             steps {
-                withCredentials([usernamePassword(credentialsId: 'jenkins', usernameVariable: 'JENKINS_USER', passwordVariable: 'JENKINS_PASS')]) {
+                withCredentials([string(credentialsId: 'jenkins-creds', variable: 'JENKINS_PASS')]) {
                     sh '''
                         set -e
-
-                        echo "=== CREATE config.ini ==="
 
                         cat > config.ini <<EOF
 [jenkins]
 url=http://89.124.113.71/jenkins/
-user=${JENKINS_USER}
+user=admin
 password=${JENKINS_PASS}
 
 [job_builder]
@@ -56,7 +30,7 @@ recursive=True
 keep_descriptions=False
 EOF
 
-                        echo "=== CONFIG CREATED ==="
+                        echo "CONFIG CREATED"
                         ls -la config.ini
                         cat config.ini
                     '''
@@ -65,54 +39,49 @@ EOF
         }
 
         stage('Host debug') {
-
-            sh '''
-    HOSTNAME=$(hostname)
-    echo "HOST=$HOSTNAME"
-'''
-
-
             steps {
                 sh '''
-                    echo "=== HOST DEBUG ==="
+                    echo "HOST DEBUG"
                     echo "WORKSPACE=$WORKSPACE"
-
                     ls -la $WORKSPACE
-
-                    echo "=== CHECK CONFIG ON HOST ==="
-                    cat $WORKSPACE/config.ini
-
-                    echo "=== CHECK JOBS DIR ==="
-                    ls -la $WORKSPACE/jobs
+                    ls -la jobs
                 '''
             }
         }
 
-        stage('Run JJB') {
-
-            sh '''
-    HOSTNAME=$(hostname)
-    echo "HOST=$HOSTNAME"
-'''
-
-
+        stage('Run Jenkins Job Builder') {
             steps {
                 sh '''
-rm -rf /tmp/jjb_workspace
-mkdir -p /tmp/jjb_workspace
+                    set -e
 
-cp -r $WORKSPACE/* /tmp/jjb_workspace/
+                    echo "=== DOCKER RUN ==="
 
-docker run --rm \
-  -v /tmp/jjb_workspace:/workspace \
-  -w /workspace \
-  python:3.10 bash -c '
-    set -e
-    ls -la
-    ls -la config.ini
-    jenkins-jobs --conf config.ini update jobs/
-  '
-'''
+                    docker run --rm \
+                      -v $WORKSPACE:/workspace \
+                      -w /workspace \
+                      python:3.10 \
+                      bash -c "
+                        set -e
+
+                        echo 'INSIDE CONTAINER'
+                        pwd
+                        ls -la
+
+                        echo 'CONFIG CHECK'
+                        ls -la config.ini
+                        cat config.ini
+
+                        echo 'JOBS'
+                        ls -la jobs
+
+                        python --version
+
+                        pip install --no-cache-dir jenkins-job-builder==5.0.3
+
+                        jenkins-jobs --version
+                        jenkins-jobs --conf config.ini update jobs/
+                      "
+                '''
             }
         }
     }
@@ -120,8 +89,8 @@ docker run --rm \
     post {
         always {
             sh '''
-                echo "=== PIPELINE FINISHED ==="
-                ls -la $WORKSPACE
+                echo "PIPELINE FINISHED"
+                ls -la
             '''
         }
     }
