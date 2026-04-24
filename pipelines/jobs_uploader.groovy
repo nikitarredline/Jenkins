8a1f353bba0402893c9a90d9a3a38561392c894b
nikitarredline <povarsemen@yandex.ru>
2026-04-24 19:12:24 +0300

    practice

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -37,12 +37,20 @@ EOF
     stage('Run JJB') {
         sh '''
         apt-get update -y
-        apt-get install -y python3.11 python3.11-venv python3-pip
+        apt-get install -y git curl build-essential libssl-dev zlib1g-dev \
+            libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
+            libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-        python3.11 -m venv venv
-        . venv/bin/activate
+        curl https://pyenv.run | bash
 
-        pip install --upgrade pip setuptools==68.0.0 wheel
+        export PATH="$HOME/.pyenv/bin:$PATH"
+        eval "$(pyenv init -)"
+        eval "$(pyenv virtualenv-init -)"
+
+        pyenv install 3.10.14
+        pyenv global 3.10.14
+
+        pip install --upgrade pip setuptools wheel
         pip install jenkins-job-builder==5.0.3
 
         jenkins-jobs --conf config.ini update jobs/
